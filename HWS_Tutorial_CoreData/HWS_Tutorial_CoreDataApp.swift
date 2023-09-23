364dfddb09313fb4c53906bd84b289faf6ab3264
Mladen Mikić <mladen.mikic@unseenexcellence.com>
2023-09-23 18:02:34 +0200

    Initial Commit

---
+++ HWS_Tutorial_CoreData/HWS_Tutorial_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_Tutorial_CoreDataApp.swift
+//  HWS_Tutorial_CoreData
+//
+//  Created by Mladen Mikić on 23.09.2023..
+//
+
+import SwiftUI
+
+@main
+struct HWS_Tutorial_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
